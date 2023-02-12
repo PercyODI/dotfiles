127b2da45e237fd82df0e9b376b7a371cabdf2c4
Pearse Hutson <pearse.2008@gmail.com>
2023-02-12 14:52:37 -0600

    Adding wezterm

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,12 @@
+local wezterm = require 'wezterm'
+
+return {
+    font = wezterm.font 'FiraCode Nerd Font Mono',
+    color_scheme = 'PencilDark',
+    window_padding = {
+        left = 0,
+        right = 0,
+        top = 0,
+        bottom = 0
+    }
+}
